a8389df939387436d01d3d93107291b894878a37
Jesús Hernández <mjesushernandez0@gmail.com>
2022-06-21 13:11:55 -0500

    config tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div>Home Page</div>;
+  return <div className="bg-red-700">Home Page</div>;
 };
 
 export default Home;
